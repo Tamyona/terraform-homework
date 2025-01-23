35927195a1e628d3a0f671cf25c00aaa371d496a
tamyona <kazkadyrbek@gmail.com>
2025-01-23 04:31:54 +0000

    modified ec2 file

--- Homework4/ec2.tf
+++ Homework4/ec2.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "homework4" {
   ami           = var.ami_id
   instance_type = var.instance_type
   subnet_id = var.az
+  key_name = aws_key_pair.deployer.key_name
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
 
   tags = {
